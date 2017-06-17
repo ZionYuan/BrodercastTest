085b088385feecb53bbf8c21da4ae1c760f4105c
yuanzi <yuanzi@gmail.com>
2017-06-17 16:44:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yuanzi/brodercasttest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.yuanzi.brodercasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver,intentFilter);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new
+                        Intent("com.example.yuanzi.brodercasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+}
---
+++ app/src/main/java/com/example/yuanzi/brodercasttest/NetworkChangeReceiver.java
@@ -0,0 +1,17 @@
+package com.example.yuanzi.brodercasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by yuanzi on 2017/6/16.
+ */
+
+class NetworkChangeReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Toast.makeText(context,"network changes",Toast.LENGTH_SHORT).show();
+    }
+}
